c2429dfb9dcf92da24cc7e83a8ff5d162c1705b6
Dylan <thor.dh@gmail.com>
2016-11-05 12:45:22 -0500

    progress

--- hacking-w-react/src/pages/Detail.js
+++ hacking-w-react/src/pages/Detail.js
@@ -5,29 +5,91 @@ class Detail extends React.Component {
   constructor(props) {
     super(props);
 
-
-  this.state = { commits: [] };
+  this.state = {
+    mode: 'commits',
+    commits: [],
+    forks: [],
+    pulls: []
+  };
 
   }
-  componentWillMount() {
-    ajax.get('https://api.github.com/repos/facebook/react/commits')
+
+  fetchFeed(type) {
+    ajax.get(`https://api.github.com/repos/facebook/react/${type}`)
       .end((error, response) => {
         if (!error && response) {
-          this.setState({ commits: response.body });
-        } else {
-          console.log('There was an error fetching from Github', error);
+          this.setState({ [type]: response.body });
+        }
+        else {
+          console.log(`Error fetching ${type}`, error)
         }
       })
   }
 
+  componentWillMount() {
+    this.fetchFeed('commits');
+    this.fetchFeed('forks');
+    this.fetchFeed('pulls');
+  }
+
+  renderCommits() {
+      return this.state.commits.map((commit, index) => {
+          const author = commit.author ? commit.author.login : 'Anonymous';
+
+          return (<p key={index}>
+              <strong>{author}</strong>:
+              <a href={commit.html_url}>{commit.commit.message}</a>.
+          </p>);
+      });
+  }
+
+  renderForks() {
+      return this.state.forks.map((fork, index) => {
+          const owner = fork.owner ? fork.owner.login : 'Anonymous';
+
+          return (<p key={index}>
+              <strong>{owner}</strong>: forked to <a href={fork.html_url}>{fork.html_url}</a> at {fork.created_at}.
+          </p>);
+      });
+  }
+
+  renderPulls() {
+      return this.state.pulls.map((pull, index) => {
+          const user = pull.user ? pull.user.login : 'Anonymous';
+
+          return (<p key={index}>
+              <strong>{user}</strong>:
+              <a href={pull.html_url}>{pull.body}</a>.
+          </p>);
+      });
+  }
+
+  handleClick(name) {
+
+    console.log(name)
+    this.setState({ mode: name })
+  }
+
+
+
   render() {
-    return (
-      <div>
-      {this.state.commits.map((commit, index) => (
-        <p key = {index}>Some commit data here.</p>
-      ))}
-      </div>
-  )}
+    let content;
+
+    if (this.state.mode === 'commits') {
+         content = this.renderCommits();
+     } else if (this.state.mode === 'forks') {
+         content = this.renderForks();
+     } else {
+         content = this.renderPulls();
+     }
+
+    return (<div>
+      <button name="commits" onClick={this.handleClick.bind(this, 'commits')}>Commits</button>
+      <button name="forks" onClick={this.handleClick.bind(this, 'forks')}>Forks</button>
+      <button name="pulls" onClick={this.handleClick.bind(this, 'pulls')}>Pulls</button>
+      {content}
+    </div>);
+}
 }
 
 export default Detail;
